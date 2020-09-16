6e85c2e3b4ab9516128ed03e75fe5ccd5b9441e0
Sadha <sadha@smcit.co.za>
2020-09-16 16:17:16 +0200

    InstantMeetings Customization v33

--- config/application.rb
+++ config/application.rb
@@ -126,7 +126,7 @@ module Greenlight
     config.maintenance_mode = ENV["MAINTENANCE_MODE"] == "true"
 
     config.report_issue_url = ENV["REPORT_ISSUE_URL"]
-    config.help_url = ENV["HELP_URL"].nil? ? "https://docs.bigbluebutton.org/greenlight/gl-overview.html" : ENV["HELP_URL"]
+    config.help_url = ENV["HELP_URL"].nil? ? "https://instantmeetings.co.za/meet" : ENV["HELP_URL"]
 
     # File types allowed in preupload presentation
     config.allowed_file_types = ".doc,.docx,.ppt,.pptx,.pdf,.xls,.xlsx,.txt,.rtf,.odt,.ods,.odp,.odg,.odc,.odi,.jpg,.jpeg,.png"
@@ -134,7 +134,7 @@ module Greenlight
     # DEFAULTS
 
     # Default branding image if the user does not specify one
-    config.branding_image_default = "https://raw.githubusercontent.com/bigbluebutton/greenlight/master/app/assets/images/logo_with_text.png"
+    config.branding_image_default = "https://instantmeetings.co.za/images/logoIM.png"
 
     # Default primary color if the user does not specify one
     config.primary_color_default = "#467fcf"
